a772520351cda3721b1c34f138fe7ad2614c9003
taeok-kim <taeok.kim@nhn.com>
2025-07-27 21:13:57 +0900

    update

--- collector/ethstats.go
+++ collector/ethstats.go
@@ -21,18 +21,29 @@ type EthStatsCollector struct {
 func NewEthStatsCollector() *EthStatsCollector {
 	labels := []string{"port"}
 	return &EthStatsCollector{
-		rxPackets: prometheus.NewDesc("ethstats_rx_packets_total", "HW RX Packets", labels, nil),
-		rxBytes:   prometheus.NewDesc("ethstats_rx_bytes_total", "HW RX Bytes", labels, nil),
-		txPackets: prometheus.NewDesc("ethstats_tx_packets_total", "HW TX Packets", labels, nil),
-		txBytes:   prometheus.NewDesc("ethstats_tx_bytes_total", "HW TX Bytes", labels, nil),
+		rxPackets: prometheus.NewDesc("vswitch_receive_packets_total", "HW RX Packets", labels, nil),
+		rxBytes:   prometheus.NewDesc("vswitch_receive_bytes_total", "HW RX Bytes", labels, nil),
+		rxMissed:  prometheus.NewDesc("vswitch_receive_missed_total", "HW RX Missed", labels, nil),
+		rxErrors:  prometheus.NewDesc("vswitch_receive_errors_total", "HW RX Errors", labels, nil),
+		rxNoMbuf:  prometheus.NewDesc("vswitch_receive_no_mem_buffer_total", "HW RX No Mbuf", labels, nil),
+		txPackets: prometheus.NewDesc("vswitch_transmit_packets_total", "HW TX Packets", labels, nil),
+		txBytes:   prometheus.NewDesc("vswitch_transmit_bytes_total", "HW TX Bytes", labels, nil),
+		txMissed:  prometheus.NewDesc("vswitch_transmit_missed_total", "HW TX Missed", labels, nil),
+		txErrors:  prometheus.NewDesc("vswitch_transmit_errors_total", "HW TX Errors", labels, nil),
 	}
 }
 
 func (c *EthStatsCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.rxPackets
 	ch <- c.rxBytes
+	ch <- c.rxMissed
+	ch <- c.rxErrors
+	ch <- c.rxNoMbuf
+
 	ch <- c.txPackets
 	ch <- c.txBytes
+	ch <- c.txMissed
+	ch <- c.txErrors
 }
 
 func (c *EthStatsCollector) Collect(ch chan<- prometheus.Metric) {
@@ -59,15 +70,34 @@ func (c *EthStatsCollector) Collect(ch chan<- prometheus.Metric) {
 		case strings.HasPrefix(line, "HW RX Packets:"):
 			val := extractValue(line)
 			ch <- prometheus.MustNewConstMetric(c.rxPackets, prometheus.CounterValue, val, port)
+			
 		case strings.HasPrefix(line, "HW RX Bytes:"):
 			val := extractValue(line)
 			ch <- prometheus.MustNewConstMetric(c.rxBytes, prometheus.CounterValue, val, port)
+
+		case strings.HasPrefix(line, "HW RX Missed:"):
+			val := extractValue(line)
+			ch <- prometheus.MustNewConstMetric(c.rxMissed, prometheus.CounterValue, val, port)
+
+		case strings.HasPrefix(line, "HW RX Errors:"):
+			val := extractValue(line)
+			ch <- prometheus.MustNewConstMetric(c.rxErrors, prometheus.CounterValue, val, port)
+
+		case strings.HasPrefix(line, "HW RX No Mbuf:"):
+			val := extractValue(line)
+			ch <- prometheus.MustNewConstMetric(c.rxNoMbuf, prometheus.CounterValue, val, port)
+
 		case strings.HasPrefix(line, "HW TX Packets:"):
 			val := extractValue(line)
 			ch <- prometheus.MustNewConstMetric(c.txPackets, prometheus.CounterValue, val, port)
-		case strings.HasPrefix(line, "HW TX Bytes:"):
+
+		case strings.HasPrefix(line, "HW TX Missed:"):
 			val := extractValue(line)
-			ch <- prometheus.MustNewConstMetric(c.txBytes, prometheus.CounterValue, val, port)
+			ch <- prometheus.MustNewConstMetric(c.txMissed, prometheus.CounterValue, val, port)
+
+		case strings.HasPrefix(line, "HW TX Errors:"):
+			val := extractValue(line)
+			ch <- prometheus.MustNewConstMetric(c.txErrors, prometheus.CounterValue, val, port)
 		}
 	}
 }
